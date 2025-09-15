975497ad4bdd3cf8ce6a1c0b82fd524e104f5455
Uncle-T36 <tchafuruka@gmail.com>
2025-09-15 19:13:07 +0200

    Add SMTP configuration example

--- smtp_config_example.py
+++ smtp_config_example.py
@@ -1,6 +1,6 @@
 smtp_cfg = {
-    "server": "smtp.gmail.com",
-    "port": 587,
-    "user": "your@email.com",
-    "password": "yourpassword"
+    'server': 'smtp.yourprovider.com',    # e.g., smtp.gmail.com
+    'port': 587,                          # 465 for SSL, 587 for TLS
+    'user': 'your@email.com',             # your email address
+    'password': 'your_email_password'     # your email password or app password
 }
