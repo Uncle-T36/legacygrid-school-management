62a851abbac0b3c50313aa85206ac8344d371804
Uncle-T36 <tchafuruka@gmail.com>
2025-09-16 19:18:32 +0200

    Update the URLs configuration

--- legacygrid_school_management/urls.py
+++ legacygrid_school_management/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from schools.views import home  # Import homepage view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("schools/", include("schools.urls")),
     path("billing/", include("billing.urls")),
+    path('', home, name='home'),  # Homepage route
 ]
