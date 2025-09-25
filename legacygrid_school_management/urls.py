65ce7d6629b66856d2c1c727ef54c8561b06e497
Uncle-T36 <tchafuruka@gmail.com>
2025-09-25 09:34:05 +0200

    Full account/profile feature fix

--- legacygrid_school_management/urls.py
+++ legacygrid_school_management/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from schools.views import home  # Import homepage view
+from schools.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("schools/", include("schools.urls", namespace="schools")),
-    path("billing/", include("billing.urls", namespace="billing")),
-    path('account/', include('account.urls', namespace='account')),  # <-- Added account app URLs
-    path('', home, name='home'),  # Homepage route
+    path('schools/', include('schools.urls', namespace='schools')),
+    path('billing/', include('billing.urls', namespace='billing')),
+    path('account/', include('account.urls', namespace='account')),
+    path('', home, name='home'),
 ]
