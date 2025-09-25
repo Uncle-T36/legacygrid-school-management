9419145443d24a8723a277a77a9a8540a7a4a933
Uncle-T36 <tchafuruka@gmail.com>
2025-09-25 18:00:31 +0200

    Add main application files

--- legacygrid_school_management/urls.py
+++ legacygrid_school_management/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from schools.views import home
 
 urlpatterns = [
@@ -9,3 +11,5 @@ urlpatterns = [
     path('account/', include('account.urls', namespace='account')),
     path('', home, name='home'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
